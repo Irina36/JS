25742451af672094c1026e25d234e3d176f7c92e
Irina Okhotina <ohotina.i@gmail.com>
2017-04-04 22:00:59 +0300

    Плюс еще один пример по if

--- Example1/scripts/operator_if.js
+++ Example1/scripts/operator_if.js
@@ -1,50 +1,72 @@
-/if..else
+    */if..else
 
-var company = prompt('Каково официальное название JS ?',' ');
-if (company =='EcmaScript') {
-  console.log('Верно!');
-  }
-  else {
-    console.log('Не знаете? "EcmaScript!"');
+    var company = prompt('Каково официальное название JS ?',' ');
+    if (company =='EcmaScript') {
+      console.log('Верно!');
+      }
+      else {
+        console.log('Не знаете? "EcmaScript!"');
 
-/operator '?'
+    /operator '?'
 
-var answer;
-var company = prompt('Каково официальное название JS ?',' ');
-answer = (company=='EcmaScript')? 'Верно!':
-'Не знаете? "EcmaScript!"';
+    var answer;
+    var company = prompt('Каково официальное название JS ?',' ');
+    answer = (company=='EcmaScript')? 'Верно!':
+    'Не знаете? "EcmaScript!"';
 
-/next example
+    /next example
 
-var question= prompt('Введите число', '');
-if (question>0) {
-  console.log(1);
-}else if (question<0) {
-  console.log(-1);
-} else {
-  console.log(0);
-};
+    var question= prompt('Введите число', '');
+    if (question>0) {
+      console.log(1);
+    }else if (question<0) {
+      console.log(-1);
+    } else {
+      console.log(0);
+    };
 
-/next example
+    /next example
 
-var message;
-var login;
+    var message;
+    var login;
 
-if (login == 'Вася') {
-  message = 'Привет';
-} else if (login == 'Директор') {
-  message = 'Здравствуйте';
-} else if (login == '') {
-  message = 'Нет логина';
-} else {
-  message = '';
-};
+    if (login == 'Вася') {
+      message = 'Привет';
+    } else if (login == 'Директор') {
+      message = 'Здравствуйте';
+    } else if (login == '') {
+      message = 'Нет логина';
+    } else {
+      message = '';
+    };
 
-/operator '?'
+    /operator '?'
 
-var login=prompt('Кто он?');
-var message=(login=='Вася') ? 'Привет':
-(login=='Директор') ? 'Здравствуйте' :
-(login=='') ? 'Нет логина':
-'';
-console.log(message);
+    var login=prompt('Кто он?');
+    var message=(login=='Вася') ? 'Привет':
+    (login=='Директор') ? 'Здравствуйте' :
+    (login=='') ? 'Нет логина':
+    '';
+    console.log(message);
+
+
+    var browser=prompt("Какой у вас браузер?");
+    if (browser=="IE") {
+      console.log("Да, у вас IE");
+    }else 
+    if (browser=="Chrom" || "Firefox" ||"Safari" || "Opera") {
+      console.log("Да, эти браузеры мы поддерживаем");
+    } else {
+      console.log ("Мы надеемся что и в вашем браузере все ОК");
+    }
+
+    */ next example
+    var browser=prompt("Какой у вас браузер?");
+    if (browser=="IE") {
+      console.log("Да, у вас IE");
+    }else 
+    if (browser=="Chrom" || "Firefox" ||"Safari" || "Opera") {
+      console.log("Да, эти браузеры мы поддерживаем");
+    } else {
+      console.log ("Мы надеемся что и в вашем браузере все ОК");
+    }
